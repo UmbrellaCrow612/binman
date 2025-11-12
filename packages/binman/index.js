113a1393b28a5dbac101bedcbc40b4a9c464ac1e
Yousaf Wazir <umbrellaman612@gmail.com>
2025-11-12 13:40:16 +0000

    build script npm package

---
+++ packages/binman/index.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const { spawn } = require("child_process");
+const path = require("path");
+const os = require("os");
+
+// Determine platform
+let binaryName;
+switch (os.platform()) {
+  case "win32":
+    binaryName = "binman.exe";
+    break;
+  case "darwin":
+    binaryName = "binman-darwin";
+    break;
+  case "linux":
+    binaryName = "binman-linux";
+    break;
+  default:
+    console.error("Unsupported platform:", os.platform());
+    process.exit(1);
+}
+
+const binaryPath = path.join(__dirname, "bin", binaryName);
+const args = process.argv.slice(2);
+
+const child = spawn(binaryPath, args, { stdio: "inherit" });
+child.on("exit", (code) => process.exit(code));

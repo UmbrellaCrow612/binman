cec1322c45ce51e75c574fd169d55c3db49edcc6
Yousaf Wazir <umbrellaman612@gmail.com>
2025-11-23 16:47:34 +0000

    got somthing working

--- cli/extractor/bin.go
+++ cli/extractor/bin.go
@@ -1,8 +1,7 @@
 package extractor
 
 import (
-	"io"
-	"io/fs"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -11,31 +10,60 @@ import (
 	"github.com/UmbrellaCrow612/binman/cli/shared"
 )
 
-// CopyToBin copies the content of the first folder with files from downloads into the bin directory
-func CopyToBin(binary *shared.Binary, options *args.Options) error {
-	baseDownloadDir := filepath.Join(options.Path, "downloads", binary.NAME)
-	baseBinDir := filepath.Join(options.Path, "bin", binary.NAME)
+func CopyToBin(bin *shared.Binary, options *args.Options) error {
+	baseDownloadDir := filepath.Join(options.Path, "downloads", bin.NAME)
+	binDir := filepath.Join(options.Path, "bin", bin.NAME)
 
-	for platform, archMap := range binary.URLS {
-		for arch := range archMap {
-			downloadArchDir := filepath.Join(baseDownloadDir, platform, arch)
+	if _, err := os.Stat(baseDownloadDir); os.IsNotExist(err) {
+		return fmt.Errorf("download folder %s not found", baseDownloadDir)
+	}
 
-			sourceContentDir, err := findContentDir(downloadArchDir)
+	for platform, archAndUrlMap := range bin.URLS {
+		for arch := range archAndUrlMap {
+			finalDownloadDir := filepath.Join(baseDownloadDir, platform, arch)
+			_, err := os.Stat(finalDownloadDir)
 			if err != nil {
-				if os.IsNotExist(err) {
-					continue
+				return fmt.Errorf("download folder %s not found", finalDownloadDir)
+			}
+
+			if err := removeArchives(finalDownloadDir); err != nil {
+				return err
+			}
+			if err := flattenFolder(finalDownloadDir); err != nil {
+				return err
+			}
+			if err := removeEmptyDirs(finalDownloadDir); err != nil {
+				return err
+			}
+
+			finalBinDir := filepath.Join(binDir, platform, arch)
+			if err := os.MkdirAll(finalBinDir, 0755); err != nil {
+				return fmt.Errorf("failed to create bin directory %s: %w", finalBinDir, err)
+			}
+
+			files, err := os.ReadDir(finalDownloadDir)
+			if err != nil {
+				return fmt.Errorf("failed to read files in %s: %w", finalDownloadDir, err)
+			}
+
+			for _, file := range files {
+				srcPath := filepath.Join(finalDownloadDir, file.Name())
+				dstPath := filepath.Join(finalBinDir, file.Name())
+
+				counter := 1
+				for {
+					if _, err := os.Stat(dstPath); os.IsNotExist(err) {
+						break
+					}
+					ext := filepath.Ext(file.Name())
+					name := file.Name()[0 : len(file.Name())-len(ext)]
+					dstPath = filepath.Join(finalBinDir, fmt.Sprintf("%s_%d%s", name, counter, ext))
+					counter++
 				}
-				return err
-			}
 
-			binArchDir := filepath.Join(baseBinDir, platform, arch)
-			err = os.MkdirAll(binArchDir, os.ModePerm)
-			if err != nil {
-				return err
-			}
-
-			if err := copyDirContents(sourceContentDir, binArchDir); err != nil {
-				return err
+				if err := os.Rename(srcPath, dstPath); err != nil {
+					return fmt.Errorf("failed to move file %s to %s: %w", srcPath, dstPath, err)
+				}
 			}
 		}
 	}
@@ -43,111 +71,84 @@ func CopyToBin(binary *shared.Binary, options *args.Options) error {
 	return nil
 }
 
-// findContentDir recursively searches a directory to find the deepest subdirectory that
-// contains actual files, skipping intermediate, unnecessary folders created by extraction.
-func findContentDir(root string) (string, error) {
-	currentDir := root
-
-	for {
-		// Read directory contents
-		entries, err := os.ReadDir(currentDir)
+// removeArchives deletes all archive files in the directory and subdirectories
+func removeArchives(root string) error {
+	archiveExts := []string{".zip", ".tar", ".tar.gz", ".tgz", ".tar.bz2", ".tar.xz"}
+	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return "", err
+			return err
 		}
 
-		var dirs []fs.DirEntry
-		fileCount := 0
+		if info.IsDir() {
+			return nil
+		}
 
-		for _, entry := range entries {
-			// Skip archives and hidden directories
-			if strings.HasPrefix(entry.Name(), ".") || strings.Contains(entry.Name(), ".tar.gz") {
-				continue
-			}
-
-			if entry.IsDir() {
-				dirs = append(dirs, entry)
-			} else {
-				fileCount++
+		lowerName := strings.ToLower(info.Name())
+		for _, ext := range archiveExts {
+			if strings.HasSuffix(lowerName, ext) {
+				if err := os.Remove(path); err != nil {
+					return fmt.Errorf("failed to remove archive %s: %w", path, err)
+				}
+				break
 			}
 		}
 
-		// Case 1: The current directory has files. This is the content directory we want.
-		if fileCount > 0 {
-			return currentDir, nil
-		}
-
-		// Case 2: The current directory only contains ONE non-hidden subdirectory and no files.
-		// This means we need to descend further.
-		if len(dirs) == 1 {
-			currentDir = filepath.Join(currentDir, dirs[0].Name())
-			continue
-		}
-
-		// Case 3: The directory is empty or has multiple subdirectories and no files.
-		// If multiple subdirectories exist, we assume this is the top level of the contents.
-		return currentDir, nil
-	}
+		return nil
+	})
 }
 
-// copyDirContents copies the contents (files and subdirectories) from src to dst.
-func copyDirContents(src, dst string) error {
-	entries, err := os.ReadDir(src)
+// flattenFolder moves all files from subdirectories to the root
+func flattenFolder(root string) error {
+	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if path == root || info.IsDir() {
+			return nil
+		}
+
+		destPath := filepath.Join(root, info.Name())
+
+		// Handle duplicates
+		counter := 1
+		for {
+			if _, err := os.Stat(destPath); os.IsNotExist(err) {
+				break
+			}
+			ext := filepath.Ext(info.Name())
+			name := info.Name()[0 : len(info.Name())-len(ext)]
+			destPath = filepath.Join(root, fmt.Sprintf("%s_%d%s", name, counter, ext))
+			counter++
+		}
+
+		return os.Rename(path, destPath)
+	})
+}
+
+// removeEmptyDirs deletes empty directories recursively
+func removeEmptyDirs(root string) error {
+	entries, err := os.ReadDir(root)
 	if err != nil {
 		return err
 	}
 
 	for _, entry := range entries {
-		srcPath := filepath.Join(src, entry.Name())
-		dstPath := filepath.Join(dst, entry.Name())
-
 		if entry.IsDir() {
-			// Recursively copy subdirectories
-			if err := os.MkdirAll(dstPath, os.ModePerm); err != nil {
-				return err
-			}
-			if err := copyDirContents(srcPath, dstPath); err != nil {
-				return err
-			}
-		} else {
-			// Copy files with permission preservation
-			if err := copyFile(srcPath, dstPath); err != nil {
+			subDir := filepath.Join(root, entry.Name())
+			if err := removeEmptyDirs(subDir); err != nil {
 				return err
 			}
 		}
 	}
+
+	entries, err = os.ReadDir(root)
+	if err != nil {
+		return err
+	}
+	if len(entries) == 0 {
+		return os.Remove(root)
+	}
+
 	return nil
-}
-
-// copyFile copies a single file from src to dst, preserving the file's permission mode.
-func copyFile(src, dst string) error {
-	// 1. Get source file info to preserve permissions later
-	info, err := os.Stat(src)
-	if err != nil {
-		return err
-	}
-
-	// 2. Open the source file
-	sourceFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer sourceFile.Close()
-
-	// 3. Create the destination file with the same permissions
-	// os.Create uses 0666, so we'll use os.OpenFile to set initial permissions based on source
-	// Note: We use 0755 as a default for new executables if we can't determine the source mode easily.
-	// For production, using the source mode is best practice.
-	destinationFile, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE|os.O_TRUNC, info.Mode().Perm())
-	if err != nil {
-		return err
-	}
-	defer destinationFile.Close()
-
-	// 4. Copy the content efficiently using io.Copy
-	if _, err = io.Copy(destinationFile, sourceFile); err != nil {
-		return err
-	}
-
-	// 5. Ensure the executable permissions are correctly applied to the destination file
-	return os.Chmod(dst, info.Mode().Perm())
 }

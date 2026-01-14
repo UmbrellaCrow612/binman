81d3356d902129f856a526802ff7a3e28dc7f945
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-14 15:37:30 +0000

    sha

--- cli/config/validator.go
+++ cli/config/validator.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"errors"
+	"regexp"
 	"slices"
 	"strings"
 
@@ -12,7 +13,7 @@ import (
 // Vaidate a parsed yml file config
 func Validate(conf *t.Config) error {
 	for _, pack := range *conf {
-		err := validatePackage(pack)
+		err := validatePackage(&pack)
 		if err != nil {
 			return err
 		}
@@ -21,14 +22,36 @@ func Validate(conf *t.Config) error {
 }
 
 // Validate a config package
-func validatePackage(p t.Package) error {
+func validatePackage(p *t.Package) error {
 	if strings.TrimSpace(p.Name) == "" {
 		return errors.New("Package name cannot be a empty string")
 	}
 
-	for platform := range p.Platforms {
+	for platform, archMap := range p.Platforms {
 		if !slices.Contains(global.ValidPlatforms, platform) {
 			return errors.New("Platform not valid: " + platform + ". Valid platforms are: " + strings.Join(global.ValidPlatforms, ", "))
+		}
+
+		for arch, asset := range archMap {
+			if !slices.Contains(global.ValidArchitectures, arch) {
+				return errors.New("Architecture not valid: " + arch + ". Valid architectures are: " + strings.Join(global.ValidArchitectures, ", "))
+			}
+
+			if strings.TrimSpace(asset.Pattern) == "" {
+				return errors.New("Pattern must be defined for " + platform + " " + arch)
+			}
+
+			_, err := regexp.Compile(asset.Pattern)
+			if err != nil {
+				return err
+			}
+
+			if strings.TrimSpace(asset.SHA256) == "" {
+				return errors.New("SHA256 must be defined for " + platform + " " + arch)
+			}
+
+			const sha256Prefix = "sha256:"
+			asset.SHA256 = strings.TrimPrefix(asset.SHA256, sha256Prefix)
 		}
 	}
 

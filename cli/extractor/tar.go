4a9a501c326c00d77ff51ef17ca2d6aaab3bbaf3
Yousaf Wazir <umbrellaman612@gmail.com>
2026-01-15 14:34:41 +0000

    Update tar.go

--- cli/extractor/tar.go
+++ cli/extractor/tar.go
@@ -3,10 +3,12 @@ package extractor
 import (
 	"archive/tar"
 	"errors"
+	"fmt"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/UmbrellaCrow612/binman/cli/console"
 )
 
 // Extract a tar file path to a a destinaion path
@@ -16,25 +18,26 @@ func ExtractTar(fromPath string, toPath string) error {
 		return err
 	}
 	if info.IsDir() {
-		return errors.New("from path is not a file " + fromPath)
+		return errors.New("from path is not a file: " + fromPath)
 	}
 
 	ext := filepath.Ext(fromPath)
 	if ext == "" || ext == "." {
-		return errors.New("from path doesnt have a file extension " + fromPath)
+		return errors.New("from path doesn't have a file extension: " + fromPath)
 	}
 
 	if ext != ".tar" {
-		return errors.New("from path is not a tar file " + fromPath)
+		return errors.New("from path is not a tar file: " + fromPath)
 	}
 
-	reader, err := os.Open(fromPath)
+	file, err := os.Open(fromPath)
 	if err != nil {
 		return err
 	}
-	defer reader.Close()
+	defer file.Close()
 
-	tr := tar.NewReader(reader)
+	tr := tar.NewReader(file)
+
 	for {
 		hdr, err := tr.Next()
 		if err == io.EOF {
@@ -43,9 +46,33 @@ func ExtractTar(fromPath string, toPath string) error {
 		if err != nil {
 			return err
 		}
-		if _, err := io.Copy(os.Stdout, tr); err != nil {
-			log.Fatal(err)
+
+		targetPath := filepath.Join(toPath, hdr.Name)
+
+		switch hdr.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(targetPath, os.FileMode(hdr.Mode)); err != nil {
+				return err
+			}
+		case tar.TypeReg:
+			if err := os.MkdirAll(filepath.Dir(targetPath), 0755); err != nil {
+				return err
+			}
+
+			outFile, err := os.OpenFile(targetPath, os.O_CREATE|os.O_WRONLY, os.FileMode(hdr.Mode))
+			if err != nil {
+				return err
+			}
+
+			if _, err := io.Copy(outFile, tr); err != nil {
+				outFile.Close()
+				return err
+			}
+			outFile.Close()
+		default:
+			console.LogWarning(fmt.Sprintf("Skipping unknown type: %c in %s\n", hdr.Typeflag, hdr.Name))
 		}
 	}
+
 	return nil
 }

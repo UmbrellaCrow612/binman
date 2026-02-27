df25f2d563828911ca97721da11bd0d8dfb56cf1
Yousaf Wazir <umbrellaman612@gmail.com>
2026-02-27 14:45:15 +0000

    fix

--- packages/umbr-binman/scripts/cli.js
+++ packages/umbr-binman/scripts/cli.js
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const arch = process.arch;
+const platform = process.platform
 
 let binName;
 if (platform === "win32") {
